89182ba9ebd40a9af512022ba999520b18a46d41
maragao <maragao@student.42.rio>
2022-06-21 16:51:38 -0300

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,24 +6,18 @@
 /*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 15:07:25 by maragao           #+#    #+#             */
-/*   Updated: 2022/06/11 14:48:55 by maragao          ###   ########.rio      */
+/*   Updated: 2022/06/17 15:42:26 by maragao          ###   ########.rio      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
-# include <fcntl.h>
-//# include <stdio.h>
 # include <stdlib.h>
 
-void	*ft_calloc(size_t size);
+size_t	ft_strlen(char *str);
 int		ft_strchr_mod(char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_join(char *bank, char *buffer);
-int		ft_strlen_mod(char *buffer);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_return_line(char *buffer);
 char	*last_static(char *bank);
 char	*ft_read_line(int fd, char *bank);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,54 +6,23 @@
 /*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 15:07:13 by maragao           #+#    #+#             */
-/*   Updated: 2022/06/11 14:11:24 by maragao          ###   ########.rio      */
+/*   Updated: 2022/06/17 15:53:44 by maragao          ###   ########.rio      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strdup(const char *s)
+size_t	ft_strlen(char *str)
 {
-	int		i;
-	char	*ptr;
-	int		len;
+	size_t	count;
 
-	len = 0;
-	while (s[len])
-		len++;
-	ptr = (char *) malloc((len + 1));
-	if (ptr == NULL)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		ptr[i] = s[i];
-		i++;
-	}
-	ptr[i] = 0;
-	return (ptr);
+	count = 0;
+	while (str[count])
+		count++;
+	return (count);
 }
 
-void	*ft_calloc(size_t size)
-{
-	void		*ptr;
-	unsigned char	*str;
-	size_t		i;
-
-	ptr = (void *) malloc(size);
-	if (!ptr)
-		return (NULL);
-	str = (unsigned char *)ptr;
-	i = 0;
-	while (i < size)
-	{
-		str[i] = 0;
-		i++;
-	}
-	return (ptr);
-}
-
-int		ft_strchr_mod(char *s)
+int	ft_strchr_mod(char *s)
 {
 	int	i;
 
@@ -69,64 +38,30 @@ int		ft_strchr_mod(char *s)
 	return (0);
 }
 
-/*char	*ft_strcpy(char *dst, const char *src)
-{
-	size_t	i;
-
-	i = 0;
-	while (src[i++])
-		dst[i] = src[i];
-	dst[i] = 0;
-	return (dst);
-}*/
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	len_dst;
-	size_t	len_src;
-
-	len_dst = 0;
-	while (dst[len_dst])
-		len_dst++;
-	len_src = 0;
-	while (src[len_src])
-		len_src++;
-	if (size == 0)
-		return (len_src);
-	if (size <= len_dst)
-		return (size + len_src);
-	i = 0;
-	while (src[i] && (len_dst + i < size - 1))
-	{
-		dst[len_dst + i] = src[i];
-		i++;
-	}
-	dst[len_dst + i] = 0;
-	return (len_src + len_dst);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*ptr;
-	size_t	len;
-	size_t	len_s1;
-	size_t	len_s2;
+	size_t	i;
+	size_t	j;
 
-	len_s1 = 0;
-	len_s2 = 0;
 	if (!s1)
-		return (NULL);
-	while (s1[len_s1])
-		len_s1++;
-	while (s2[len_s2])
-		len_s2++;
-	len = len_s1 + len_s2 + 1;
-	ptr = (char *)ft_calloc(len);
+	{
+		s1 = malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	ptr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!ptr)
 		return (NULL);
-	ft_strlcat(ptr, s1, len);
-	ft_strlcat (ptr, s2, len);
+	i = -1;
+	while (s1[++i])
+		ptr[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	ptr[i] = '\0';
+	free (s1);
 	return (ptr);
 }
-
